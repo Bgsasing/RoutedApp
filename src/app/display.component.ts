e869a25df8e411816d3e5d2fd8b08d47ea476307
Flayre <bgsasing18@yahoo.com>
2017-09-15 09:36:28 +0800

    1

---
+++ src/app/display.component.ts
@@ -0,0 +1,18 @@
+OnInit {
+
+    studNo: number;
+    studFname: string;
+    studLname: string;
+    studProg: string;
+    studYr: number;
+}
+
+constructor(private common: ){ }
+
+ngOnInit() {
+    this.studNo =
+    this.studFname =
+    this.studLname =
+    this.studProg = 
+    this.studYr
+}
